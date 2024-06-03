c726c50aa20fd0b1bcc7e5154a08eba8dff9b2a1
Mariahcpgo <162521119+Mariahcpgo@users.noreply.github.com>
2024-06-03 17:32:03 -0300

    Correções

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navBar/NavBar'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
-import ListaTemas from './components/tema/listatemas/ListaTemas'
-import FormTema from './components/tema/formtema/FormTema'
-import DeletarTema from './components/tema/deletartema/DeletarTema'
-import { AuthProvider } from './context/AuthContext'
-import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens'
-import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem'
-import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Perfil from './pages/perfil/Perfil'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navBar/NavBar';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import ListaTemas from './components/tema/listatemas/ListaTemas';
+import FormTema from './components/tema/formtema/FormTema';
+import DeletarTema from './components/tema/deletartema/DeletarTema';
+import { AuthProvider } from './context/AuthContext';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Perfil from './pages/perfil/Perfil';
 
 function App() {
     return (
